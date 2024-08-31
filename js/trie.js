0d5aa8ad8267175361c49f0fac6ab509faf8e0d5
IPrashamsaPrabhu <iprashamsaprabhu.brigade@gmail.com>
2024-08-31 23:30:29 +0530

    adding files

---
+++ js/trie.js
@@ -0,0 +1,45 @@
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+        node.isEndOfWord = true;
+    }
+
+    autocomplete(prefix) {
+        let node = this.root;
+        for (let char of prefix) {
+            if (!node.children[char]) {
+                return [];
+            }
+            node = node.children[char];
+        }
+        return this._getWords(node, prefix);
+    }
+
+    _getWords(node, prefix) {
+        let results = [];
+        if (node.isEndOfWord) {
+            results.push(prefix);
+        }
+        for (let char in node.children) {
+            results = results.concat(this._getWords(node.children[char], prefix + char));
+        }
+        return results;
+    }
+}
